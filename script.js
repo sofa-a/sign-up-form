832f314d944f0d729c0f282eb36b4f17ec84cff2
Sophia <20864846@student.curtin.edu.au>
2024-11-22 14:15:44 +0800

    completed the project

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const form = document.querySelector('form')
 
 form.addEventListener("submit", function(event) {
-    console.log(form.password.value.trim())
-    console.log(form.confirm-password.value.trim())
-    if (form.password.value.trim() !== form.confirm-password.value.trim()) {
+    const password = form.password.value.trim();
+    const confirmPassword = form["confirm-password"].value.trim();
+    console.log("osdsf");
+    if (password !== confirmPassword) {
         event.preventDefault();
         document.querySelector(".password-error").style.display = "inline";
     }
